4e28c1771da29002136590c2a6efca3ce9b531f7
esneider-iMac <sneidergv@gmail.com>
2021-11-20 10:19:13 -0500

    CORS implemented

--- flask-api/app.py
+++ flask-api/app.py
@@ -2,10 +2,12 @@
 """ Setting up flask and database """
 
 from flask import Flask
+from flask_cors import CORS
 from routes.books import books
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@127.0.0.1:3306/books'
 
