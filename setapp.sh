44f4f829bd38e9f37ba7449c9c2aea00070a81fd
esneider-iMac <sneidergv@gmail.com>
2021-11-22 01:49:41 -0500

    mv requirements.txt => flask-api/

--- setapp.sh
+++ setapp.sh
@@ -7,5 +7,5 @@ sudo docker exec flask-api python3 api/index.py
 
 sudo docker run -d -it --network="host" -v $(pwd)/angular-ui:/ui -p 4200:4200 --name angular-ui node:16.13.0-alpine3.12
 sudo docker exec angular-ui npm install -g @angular/cli
-sudo docker exec angular ui cd ui
+sudo docker exec angular-ui cd ui
 sudo docker exec angular-ui ng serve
