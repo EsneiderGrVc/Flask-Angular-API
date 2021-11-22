e94b305b8417d45596d329c83c348bbaacbac748
esneider-iMac <sneidergv@gmail.com>
2021-11-22 01:51:59 -0500

    mv requirements.txt => flask-api/

--- setapp.sh
+++ setapp.sh
@@ -2,10 +2,9 @@
 sudo docker run -d -it --network="host" -p 4000:4000 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=books --name mysql-db mysql
 
 sudo docker run -d -it --network="host" -v $(pwd)/flask-api:/api -p 3000:3000 --name flask-api python
-sudo docker exec flask-api pip3 install -r requirements.txt
+sudo docker exec flask-api pip3 install -r api/requirements.txt
 sudo docker exec flask-api python3 api/index.py
 
 sudo docker run -d -it --network="host" -v $(pwd)/angular-ui:/ui -p 4200:4200 --name angular-ui node:16.13.0-alpine3.12
 sudo docker exec angular-ui npm install -g @angular/cli
-sudo docker exec angular-ui cd ui
-sudo docker exec angular-ui ng serve
+sudo docker exec angular-ui ui/ng serve
