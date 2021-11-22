5c6120ae0cec75aea90a34907d173eb340e05de2
esneider-iMac <sneidergv@gmail.com>
2021-11-22 02:14:14 -0500

    change cd command

--- setup-ui.sh
+++ setup-ui.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo docker run -d -it --network="host" -v $(pwd)/angular-ui:/ui -p 4200:4200 --name angular-ui node:16.13.0-alpine3.12
 sudo docker exec angular-ui npm install -g @angular/cli
-sudo docker exec angular-ui "cd /ui"
+sudo docker exec angular-ui "cd ui"
 sudo docker exec angular-ui "ng serve"
