0c3fb72eb57010f04d546885502c78f39ee715a8
esneider-iMac <sneidergv@gmail.com>
2021-11-22 02:24:23 -0500

    dir for ng serve

--- setup-ui.sh
+++ setup-ui.sh
@@ -2,4 +2,4 @@
 sudo docker run -d -it --network="host" -v $(pwd)/angular-ui:/ui -p 4200:4200 --name angular-ui node:16.13.0-alpine3.12
 sudo docker exec angular-ui npm install -g @angular/cli
 sudo docker exec angular-ui cd ui
-sudo docker exec angular-ui ng serve
+sudo docker exec angular-ui /ui/ng serve
