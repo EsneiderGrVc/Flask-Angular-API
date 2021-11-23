343163687d4f2747c71290537230c5d63e231088
esneider-iMac <sneidergv@gmail.com>
2021-11-23 12:19:36 -0500

    writting setup.sh

--- setup-api.sh
+++ setup-api.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-sudo docker run -d -it --network="host" -p 4000:4000 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=books --name mysql-db mysql
+cd flask-api/ && sudo docker build -t flask-api .
+cd ..
+sudo docker up -d
 
-sudo docker run -d -it --network="host" -v $(pwd)/flask-api:/api -p 3000:3000 --name flask-api python
-sudo docker exec flask-api pip3 install -r api/requirements.txt
-sudo docker exec flask-api python3 api/index.py
